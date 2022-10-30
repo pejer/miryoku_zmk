cf7a69fdd3dd22c20141339d8a7b53e03530ecd6
Henrik Pejer <henrik.pejer@digitalistgroup.com>
2022-10-30 12:07:32 +0100

    radell-zmk: Updates to make a good-enough keymap for mr Rådell

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_COLEMAKDH
+#define MIRYOKU_TAP_COLEMAKDH
+#define MIRYOKU_EXTRA_QWERTY
+#define MIRYOKU_NAV_INVERTED
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
