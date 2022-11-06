fd71ba8914242d0fbea4af739e9c5781960cb964
Henrik Pejer <henrik.pejer@digitalistgroup.com>
2022-11-06 16:48:19 +0100

    pejer: Formating & fixing sym layer. Left is last bits of alphas + num. And probably the rest of the layers :p

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,14 +9,21 @@
 
 // redefine base QWERTY to include semicolon - thus swedish keyboard layout (boooring I know but still)
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,  &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,  &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,  &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, AMPS),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_SYM \
-&kp LBRC,          &kp CRRT,            &kp LS(BSLH),       &kp LS(NUM_8),          &kp LS(RA(NUM_8)),          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp COLON,         &kp RA(NUM_4),       &kp LS(NUM_5),      &kp LS(RBKT),           &kp MINUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TILDE,         &kp LS(NUM_1),       &kp RA(NUM_2),      &kp LS(NUM_3),          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
-U_NP,              U_NP,                &kp LS(NUM_8),           &kp LS(NUM_9),               &kp UNDER,         U_NA,              U_NA,              U_NA,     U_NP,              U_NP
+&kp LS(RA(NUM_8)),  &kp CRRT,       &kp LS(BSLH), &kp LS(NUM_8), &kp LS(RA(NUM_9)), U_NA,  &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp COLON,          &kp RA(NUM_4),  &kp LS(NUM_5),&kp LS(RBKT),  &kp MINUS,         U_NA,  &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp RA(RBKT),       &kp LS(NUM_1),  &kp RA(NUM_2),&kp LS(NUM_3), &kp RA(NUM_7),     U_NA,  &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,                &kp LS(NUM_8),           &kp LS(NUM_9),     &kp UNDER,  U_NA,              U_NA,              U_NA,     U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NUM \
+&kp LBKT,          &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp NUM_4,         &kp NUM_5,         &kp NUM_6,         &kp EQL,           U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp GRAVE,         &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp  COLON,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp NUM_0,         &kp SLASH,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
